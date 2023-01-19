7f0ea431825a55407a8b233cc821f8b1aae7cc5e
jakobakoba <jakobakoba@gmail.com>
2023-01-19 23:19:44 +0600

    mainFragment added

--- app/src/main/java/com/example/weatherapp2/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp2/MainActivity.kt
@@ -1,42 +1,20 @@
 package com.example.weatherapp2
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-import com.example.weatherapp2.databinding.ActivityMainBinding
-import org.json.JSONObject
+import com.example.weatherapp2.fragments.MainFragment
 
 const val API_KEY = "d6ea3b8e81ac4026af1150822231801"
+
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.bGet.setOnClickListener{
-            getResult("Almaty")
-        }
+        setContentView(R.layout.activity_main)
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.placeHolder, MainFragment.newInstance())
+            .commit()
     }
-    private fun getResult (name: String) {
-        val url = "https://api.weatherapi.com/v1/current.json" +
-                "?key=$API_KEY&q=$name&aqi=no"
-        val queue = Volley.newRequestQueue(this)
-        val stringRequest = StringRequest(
-            Request.Method.GET,
-            url,
-            {
-                    response ->
-                val obj = JSONObject(response)
-                val temperature = obj.getJSONObject("current")
-                Log.d("MyLog", "Response: ${temperature.getString("temp_c")}")
-            },
-            {
-                Log.d("MyLog", "Volley error: $it")
-            }
-            )
-        queue.add(stringRequest)
-    }
+
 }
---
+++ app/src/main/java/com/example/weatherapp2/fragments/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.example.weatherapp2.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.weatherapp2.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
