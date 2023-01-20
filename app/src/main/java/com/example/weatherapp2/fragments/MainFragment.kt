6d573daafb0548c317a4134600ba5366aeacf974
jakobakoba <jakobakoba@gmail.com>
2023-01-21 00:18:33 +0600

    location access added

--- app/src/main/java/com/example/weatherapp2/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp2/fragments/MainFragment.kt
@@ -1,14 +1,19 @@
 package com.example.weatherapp2.fragments
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import com.example.weatherapp2.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
     override fun onCreateView(
@@ -18,6 +23,24 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher =registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity,"Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     companion object {
