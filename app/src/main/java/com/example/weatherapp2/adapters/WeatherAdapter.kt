8ac4d9e5db3ef1c47579546ecd280e13433ee1f7
jakobakoba <jakobakoba@gmail.com>
2023-01-23 15:01:49 +0600

    viewholder created

---
+++ app/src/main/java/com/example/weatherapp2/adapters/WeatherAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.weatherapp2.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp2.databinding.ListItemBinding
+
+class WeatherAdapter : ListAdapter<WeatherModel,WeatherAdapter.Holder>() {
+    class Holder (view: View) : RecyclerView.ViewHolder(view) {
+        val binding = ListItemBinding.bind(view)
+        fun bind(item:WeatherModel) = with(binding){
+            tvDate.text = item.time
+            tvCondition.text = item.condition
+            tvTemp.text = item.currentTemp
+
+        }
+
+
+    }
+
+}
